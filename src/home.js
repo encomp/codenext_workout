bde2b18bfdf478214c6534bd36d41ffb59cb4f1d
encomp <edgar.martinez.rico@gmail.com>
2020-10-25 14:04:18 -0700

    Render the page only if the user is logged in.

--- src/home.js
+++ src/home.js
@@ -1,7 +1,13 @@
 import 'bootstrap';
 import './scss/index.scss';
 import './scss/login.scss';
+import * as firebase from 'firebase/app';
 import { auth } from './services/firebaseService'
-import {ExerciseCardComponent} from './components/exercise_card/exerciseCard.component';
+import { ExerciseCardComponent } from './components/exercise_card/exerciseCard.component';
 
-ExerciseCardComponent.init();
+// Make the page visible only when the user is logged in.
+firebase.auth().onAuthStateChanged(function (newuser) {
+    if (newuser) {
+        ExerciseCardComponent.init();
+    }
+});
