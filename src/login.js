a7102ffbf92586bcbfa8e3b8e014a36e699d21c9
encomp <edgar.martinez.rico@gmail.com>
2020-10-25 14:03:29 -0700

    Redirect the use to home page if is already loged in.

--- src/login.js
+++ src/login.js
@@ -1,7 +1,16 @@
 import 'bootstrap';
 import './scss/index.scss';
 import './scss/login.scss';
+import * as firebase from 'firebase/app';
 import { auth } from './services/firebaseService'
-import {LoginButtonsComponent} from './components/login_buttons/loginButtons.component';
+import { LoginButtonsComponent } from './components/login_buttons/loginButtons.component';
+import { gotoHome } from './util/redirect'
 
 LoginButtonsComponent.init();
+
+//If the user is already logged in redirect to the home page.
+firebase.auth().onAuthStateChanged(function (newuser) {
+    if (newuser) {
+        gotoHome();
+    }
+});
