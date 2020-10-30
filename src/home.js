4aa8efdd7c62b2814241973cafe1bea5e29a39d4
encomp <edgar.martinez.rico@gmail.com>
2020-10-29 23:58:58 -0700

    Add the new today card component.

--- src/home.js
+++ src/home.js
@@ -4,10 +4,12 @@ import './scss/login.scss';
 import * as firebase from 'firebase/app';
 import { auth } from './services/firebaseService'
 import { ExerciseCardComponent } from './components/exercise_card/exerciseCard.component';
+import { TodayCardComponent } from './components/today_card/todayCard.component';
 
 // Make the page visible only when the user is logged in.
 firebase.auth().onAuthStateChanged(function (newuser) {
     if (newuser) {
         ExerciseCardComponent.init(newuser);
+        TodayCardComponent.init(newuser);
     }
 });
