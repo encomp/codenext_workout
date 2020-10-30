ff01d18946da4fea486bc90359055ea801f25b2d
encomp <edgar.martinez.rico@gmail.com>
2020-10-29 22:07:16 -0700

    Add a new util image file for the images.

--- src/components/exercise_select/exerciseSelect.component.js
+++ src/components/exercise_select/exerciseSelect.component.js
@@ -1,19 +1,10 @@
 import { exerciseSelectTemplate } from './exerciseSelect.template';
+import { imageMap } from './../../util/image';
 
 export const ExerciseSelectComponent = {
 
     render(model) {
-        model.imageMap = new Map();
-        let imageMap = model.imageMap;
-        imageMap['abs'] = 'assets/icons8-abs-50.png';
-        imageMap['back'] = 'assets/icons8-back-50.png';
-        imageMap['biceps'] = 'assets/icons8-biceps-50.png';
-        imageMap['chest'] = 'assets/icons8-chest-50.png';
-        imageMap['calves'] = 'assets/icons8-calves-50.png';
-        imageMap['forearms'] = 'assets/icons8-forearm-50.png';
-        imageMap['quadriceps'] = 'assets/icons8-quadriceps-50.png';
-        imageMap['shoulders'] = 'assets/icons8-shoulders-50.png';
-        imageMap['triceps'] = 'assets/icons8-triceps-50.png';
+        model.imageMap = imageMap;
         return exerciseSelectTemplate(model);
     },
 
---
+++ src/util/image.js
@@ -0,0 +1,10 @@
+export const imageMap = new Map();
+imageMap['abs'] = 'assets/icons8-abs-50.png';
+imageMap['back'] = 'assets/icons8-back-50.png';
+imageMap['biceps'] = 'assets/icons8-biceps-50.png';
+imageMap['chest'] = 'assets/icons8-chest-50.png';
+imageMap['calves'] = 'assets/icons8-calves-50.png';
+imageMap['forearms'] = 'assets/icons8-forearm-50.png';
+imageMap['quadriceps'] = 'assets/icons8-quadriceps-50.png';
+imageMap['shoulders'] = 'assets/icons8-shoulders-50.png';
+imageMap['triceps'] = 'assets/icons8-triceps-50.png';
