7c5f1f85cc4a589cf7aa4cedabe5c1bc84ba53a4
encomp <edgar.martinez.rico@gmail.com>
2020-10-31 10:14:43 -0700

    Add functionality to remove properly.

--- src/components/today_item_detail/todayItemDetail.component.js
+++ src/components/today_item_detail/todayItemDetail.component.js
@@ -1,5 +1,9 @@
 import { todayItemDetailTemplate } from './todayItemDetail.template';
+import { TodayItemComponent } from './../today_item/todayItem.component';
 import { getExerciseRef, updateExercise } from './../../repository/exercises';
+import { getMetaDataRef, getExerciseDoc } from './../../repository/metadata';
+import { firestore } from './../../services/firebaseService';
+import { disableExcercise, shouldDeleteExcercises } from './../../util/exercise';
 
 export const TodayItemDetailComponent = {
     render(model, index) {
@@ -27,24 +31,50 @@ export const TodayItemDetailComponent = {
         });
         const deleteBtn = document.querySelector('#' + name + 'BtnDelete');
         deleteBtn.addEventListener('click', event => {
+            const docRef = getExerciseRef(model.id, model.user, model.date);
             if (model.data.repetitions.length == 1) {
-                
+                const metadataRef = getMetaDataRef(model.user, model.date);
+                const data = disableExcercise(model.id);
+                const batch = firestore.batch();
+                batch.delete(docRef);
+                batch.update(metadataRef, data);
+                batch.commit()
+                    .then(function () {
+                        TodayItemComponent.remove(model);
+                        // Check if there metada should be removed.
+                        getExerciseDoc(model.user, model.date).then(function (doc) {
+                            if (doc.exists) {
+                                const exercisesData = doc.data();
+                                // Check if all the exercise are set to false.
+                                if (shouldDeleteExcercises(exercisesData)) {
+                                    metadataRef.delete().then(function () {
+                                        console.log("Document successfully deleted!", exercisesData);
+                                    }).catch(function (error) {
+                                        console.error("Error removing document: ", error);
+                                    });
+                                }
+                            }
+                        }).catch(function (error) {
+                            console.error("Error getting document:: ", error);
+                        });
+                    }).catch(function (error) {
+                        console.error("Error updating documents: ", error);
+                    });
             } else {
+                const todayItemDetail = document.querySelector('#' + name);
                 let rep = model.data.repetitions;
                 let wei = model.data.weights;
                 model.data.repetitions = rep.slice(0, index).concat(rep.slice(index + 1, rep.length));
                 model.data.weights = wei.slice(0, index).concat(wei.slice(index + 1, wei.length));
                 console.log(model.id, model.user, model.date, model.data);
-                const docRef = getExerciseRef(model.id, model.user, model.date);
                 updateExercise(docRef, model.data)
                     .then(function () {
-                        console.log("Document successfully updated!");
+                        todayItemDetail.innerHTML = '';
+                        TodayItemComponent.updateBadge(model);
                     }).catch(function (error) {
                         console.error("Error updating document: ", error);
                     });
             }
-            const row = document.querySelector('#' + name);
-            row.innerHTML = '';
         });
     },
 }
