60485f71c0a3b786a003ae8606aba39691691988
encomp <edgar.martinez.rico@gmail.com>
2020-10-31 16:51:58 -0700

    Add a new function to extract the doc.

--- src/repository/exercises.js
+++ src/repository/exercises.js
@@ -4,6 +4,10 @@ export function getExerciseRef(exercise, userEmail, newDate) {
     return firestore.collection(exercise).doc(userEmail).collection("dates").doc(newDate);
 }
 
+export function getExercise(exercise, userEmail, newDate) {
+    return firestore.collection(exercise).doc(userEmail).collection("dates").doc(newDate).get();
+}
+
 export function saveExercise(exercise, user, newDate, data) {
     return firestore.collection(exercise)
         .doc(user)
