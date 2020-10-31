1b402094c09a3657a40b397ca0a1c70f6ceee390
encomp <edgar.martinez.rico@gmail.com>
2020-10-31 10:05:59 -0700

    Add a get function to retrieve an exercise.

--- src/repository/metadata.js
+++ src/repository/metadata.js
@@ -5,6 +5,10 @@ export function getMetaDataRef(userEmail, newDate) {
     return firestore.collection("exercises").doc(userEmail).collection("dates").doc(newDate);
 }
 
+export function getExerciseDoc(userEmail, newDate) {
+    return firestore.collection("exercises").doc(userEmail).collection("dates").doc(newDate).withConverter(converterExercises).get();
+}
+
 export function saveMetaData(userEmail, newDate, data) {
     return firestore.collection("exercises")
         .doc(userEmail)
