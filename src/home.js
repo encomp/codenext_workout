894af1667fc2e5f0f52978ac1271cf64e4897afd
encomp <edgar.martinez.rico@gmail.com>
2020-10-31 21:03:01 -0700

    Add a log out button onclick action.

--- src/home.js
+++ src/home.js
@@ -6,13 +6,26 @@ import { auth } from './services/firebaseService'
 import { ExerciseCardComponent } from './components/exercise_card/exerciseCard.component';
 import { TodayCardComponent } from './components/today_card/todayCard.component';
 import { getDate } from './util/date';
+import { gotoIndex } from './util/redirect'
 
 // Make the page visible only when the user is logged in.
 firebase.auth().onAuthStateChanged(function (newuser) {
     if (newuser) {
+        const logOutBtn = document.getElementById('btnLogOut');
+        logOutBtn.addEventListener('click', event => {
+            firebase.auth().signOut().then(function () {
+                gotoIndex();
+            }).catch(function (error) {
+                console.error("Unable to logout.", error);
+            });
+        });
+
         const forDate = getDate();
         const todayCardId = '#cardTodayProgress';
         ExerciseCardComponent.init('#cardExercise', newuser.email, todayCardId, forDate);
         TodayCardComponent.init(todayCardId, newuser.email, forDate);
+
+    } else {
+        gotoIndex();
     }
 });
