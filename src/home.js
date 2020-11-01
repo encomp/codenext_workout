7c474c203b08d56b8a6aad57b75308f2fbb53519
encomp <edgar.martinez.rico@gmail.com>
2020-10-31 21:05:33 -0700

    Refactor to make code cleaner.

--- src/home.js
+++ src/home.js
@@ -10,6 +10,17 @@ import { gotoIndex } from './util/redirect'
 // Make the page visible only when the user is logged in.
 firebase.auth().onAuthStateChanged(function (newuser) {
     if (newuser) {
+        firebase.auth().setPersistence(firebase.auth.Auth.Persistence.SESSION)
+            .then(function () {
+                return;
+            })
+            .catch(function (error) {
+                var errorCode = error.code;
+                var errorMessage = error.message;
+                console.error(errorCode, errorMessage);
+            });
+
+
         const logOutBtn = document.getElementById('btnLogOut');
         logOutBtn.addEventListener('click', event => {
             firebase.auth().signOut().then(function () {
