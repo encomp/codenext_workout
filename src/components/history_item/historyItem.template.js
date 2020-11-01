ad8517de7bfd41efdb388d74303ca04e8781916c
encomp <edgar.martinez.rico@gmail.com>
2020-11-01 00:35:58 -0700

    Add exercies.

--- src/components/history_item/historyItem.template.js
+++ src/components/history_item/historyItem.template.js
@@ -1,7 +1,12 @@
+import {imageMap} from './../../util/image';
+
 export const historyItemTemplate = model => `
 <div id="${model.id}Card" class="card">
     <div class="card-header text-dark d-flex justify-content-between align-items-center" id="headingOne">
         <h5 class="font-weight-normal">${model.id}</h5>
+    </div>
+    <div class="card-body d-flex justify-content-between align-items-center">
+        ${model.exercise.enabledExercises().map(element => '<img src="' + imageMap[element] + '"></img>').join('')}
     </div>
 </div>
 `;
