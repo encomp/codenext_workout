e2e2eadd117db78b12ceeb6fe98b46fd68e64424
encomp <edgar.martinez.rico@gmail.com>
2020-11-01 20:18:58 -0800

    Fix the index page to rederict to login.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,2 @@
 import 'bootstrap';
 import './scss/index.scss';
-import { gotoLogin } from './util/redirect';
-
-const enterBtn = document.getElementById('enterBtn');
-enterBtn.addEventListener('click', event => {
-    gotoLogin();
-});
